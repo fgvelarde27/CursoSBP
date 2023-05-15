e2e9d90c78fec308e9bae6f547d1237873f242bd
VELARDE, FELIX <fvelarde@superbancos.gob.pa>
2023-05-15 13:37:00 -0500

    create datacontext and model Basic Project.

---
+++ CursoSBP.Common/Services/ApiServices.cs
@@ -0,0 +1,32 @@
+﻿using CursoSBP.Common.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoSBP.Common.Services
+{
+    public class ApiServices : IApiServices
+    {
+        public Response DeleteApi(string url, string controller, string endpoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response GetApi(string url, string controller, string endpoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response PostApi(string url, string controller, string endpoint)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response PutApi(string url, string controller, string endpoint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CursoSBP.Common/Services/IApiServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CursoSBP.Common.Models.ViewModels;
+
+namespace CursoSBP.Common.Services
+{
+    public interface IApiServices
+    {
+       Response GetApi(string url, string controller, string endpoint);
+        Response PostApi(string url, string controller, string endpoint);
+        Response PutApi(string url, string controller, string endpoint);
+        Response DeleteApi(string url, string controller, string endpoint);
+    }
+}
